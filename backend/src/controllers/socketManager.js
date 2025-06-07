695a824bfec16208ce711eecdc9d6236548450f9
bisu <khatuasouvik647@gmail.com>
2025-06-08 00:27:02 +0530

    lastway

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -29,9 +29,7 @@ export const connectToSocket = (server) => {
 
             timeOnline[socket.id] = new Date();
 
-            // connections[path].forEach(elem => {
-            //     io.to(elem)
-            // })
+          
 
             for (let a = 0; a < connections[path].length; a++) {
                 io.to(connections[path][a]).emit("user-joined", socket.id, connections[path])
